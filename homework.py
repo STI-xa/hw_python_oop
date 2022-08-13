c51b341f1a6d0b5bddce81c5fde1defb767d82bc
Полина Николаева <sti-xa@ya.ru>
2022-08-13 12:56:33 +0300

    fixed typing

--- homework.py
+++ homework.py
@@ -148,7 +148,7 @@ class Swimming(Training):
 
 def read_package(workout_type: str, data: list) -> Training:
     """Прочитать данные полученные от датчиков."""
-    training_type: Dict[str[Type[Training]]] = {
+    training_type: Dict[str, Type[Training]] = {
         "SWM": Swimming,
         "RUN": Running,
         "WLK": SportsWalking,
